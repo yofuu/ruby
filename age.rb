8ecbd479784226477a92265f2f351a44ab58429a
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-07-27 10:16:16 +0000

    ruby練習

---
+++ age.rb
@@ -0,0 +1,23 @@
+# puts "Samの年齢は" + 27.to_s + "です"
+# "Samの年齢は" : 文字列型
+# 27 : 整数型
+#"です" : 文字列型
+
+# def withdraw(balance, amount)
+#   fee = 110
+#   if balance >= (amount + fee)
+#     balance -= (amount + fee)
+#     puts "#{amount}円引き落としました。残高は#{balance}円です"
+#   else
+#     puts "残高不足です"
+#   end
+# end
+
+# balance = 100000
+# puts "いくら引き落としますか？（手数料110円かかります）"
+# money = gets.to_i
+# withdraw(balance, money)
+
+if then 1<=2n+1<=111;
+ puts "(n)**3"
+end
