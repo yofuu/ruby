14a42a351f8aebdcc8712605f94d4e9225409ca5
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-08-01 15:28:36 +0000

    if文練習

--- break.rb
+++ break.rb
@@ -3,7 +3,7 @@ while i<=10 do
   if i==5
     puts "処理を終了します"
     break
- end
+  end
  puts i
  i += 1
 end
--- if.rb
+++ if.rb
@@ -3,3 +3,11 @@ total = 200
 if total < 300
   puts "合計は300未満です"
 end
+
+age = 21
+
+if age >= 30
+  puts "おじさん"
+else
+  puts "子供"
+end
