415ac41f562a6b976294aa7bf020d9ad901cb0f6
EC2 Default User <ec2-user@ip-172-31-25-109.ap-northeast-1.compute.internal>
2023-08-26 14:49:54 +0000

    ruby練習問題

--- practice.rb
+++ practice.rb
@@ -1,8 +1,13 @@
 num1 = "3"
 num2 = "2"
 
-puts "num1は,"+num1+"num2は,"+num2+"合計すると"+("3 + 2")+"になります。"
+puts "num1は,"+num1+"num2は,"+num2+"合計すると"+(3 + 2).to_s+"になります。"
 
 #puts "num1は"+num1+",num2は"+num2+"２つの差は"+ (num1 - num2)+"になります。"
 
-puts "num1は"+num1+",num2は"+num2+",２つの積は"+ (3 * 2) +"になります。"
+puts "num1は"+num1+",num2は"+num2+",２つの積は"+(3 * 2).to_s+"になります。"
+
+
+num1 = "8"
+num2 = "4"
+puts "num1は"+num1+",num2は"+num2+",２つの商は"+(8 % 4).to_s+"になります。"
